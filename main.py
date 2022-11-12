c7ee331d5b54aae68b9e3c9615ab7004cc8e28c0
Ike Callaghan (1000106521) <CALLIT1@student.op.ac.nz>
2022-11-12 15:31:51 +1300

    PRINTING VALUES YEAH

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Detect the faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    print(faces)
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        print(x, y)
     # Display
     cv2.imshow('img', img)
     # Stop if escape key is pressed
